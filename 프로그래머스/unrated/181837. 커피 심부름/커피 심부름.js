ba775ea14c703de012a2e83e71d1a8fc7a475635
이아론 <105274015+mini-aron@users.noreply.github.com>
2023-08-29 23:15:26 +0900

    [unrated] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,9 +1,4 @@
 function solution(order) {
     var answer = 0;
-    order.forEach((i)=>{
-        if(i.includes("cafelatte")) answer+=5000;
-        else if(i.includes("americano")) answer+=4500;
-        else answer += 4500;
-    })
     return answer;
 }
