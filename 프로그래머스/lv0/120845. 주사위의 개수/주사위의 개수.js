728366fff761e47d7b48e4ffe82730f098f9fe28
이아론 <105274015+mini-aron@users.noreply.github.com>
2023-09-27 13:55:48 +0900

    [level 0] Title: 주사위의 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,4 @@
 function solution(box, n) {
-    return box[0]<n||n===0?0:Math.floor(box[2]/n)*(Math.floor(box[0]/n)*Math.floor(box[1]/n))
+     let [width, length, height] = box;
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
 }
