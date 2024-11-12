edf268aee784afc1aefcef77e9513bc84b9ef44c
이아론 <aronia.dev@gmail.com>
2024-11-12 09:05:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,7 +1,1 @@
-function solution(n, arr1, arr2) {
-    var answer = [];
-    for(let i = 0;i<n;i++){
-    answer.push((parseInt(arr1[i])|parseInt(arr2[i])).toString(2).padStart(n," ").replace(/1/g,"#").replace(/0/g," "))
-    }
-    return answer;
-}
+const solution = (n, arr1, arr2) => arr1.map((arr,idx)=>(arr|arr2[idx]).toString(2).padStart(n," ").replace(/1/g,"#").replace(/0/g," "))
